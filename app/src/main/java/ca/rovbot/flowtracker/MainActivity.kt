4b114da33f90bc4eec89494b4126e9e8108adb0b
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-12-24 13:54:02 -0500

    Jetpack Navigation Implemeted

--- app/src/main/java/ca/rovbot/flowtracker/MainActivity.kt
+++ app/src/main/java/ca/rovbot/flowtracker/MainActivity.kt
@@ -2,18 +2,29 @@ package ca.rovbot.flowtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import ca.rovbot.flowtracker.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navHost: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        navHost = supportFragmentManager
+            .findFragmentById(R.id.navHost) as NavHostFragment
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navHost.navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
