55db6d72fa9eb58361ac8882a697c3684c15b3ce
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-12-26 12:02:05 -0500

    Navigation Stack Implemented

--- app/src/main/java/ca/rovbot/flowtracker/fragment/LogDateFragment.kt
+++ app/src/main/java/ca/rovbot/flowtracker/fragment/LogDateFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import ca.rovbot.flowtracker.viewmodel.LogDateViewModel
 import ca.rovbot.flowtracker.R
 import kotlinx.android.synthetic.main.log_date_fragment.*
@@ -66,9 +67,8 @@ class LogDateFragment : Fragment() {
 
         updateregistration.setOnClickListener{
             viewModel.updateNewPeriodDate(dateinputtext.text.toString(),daysinputtext.text.toString(), this!!.context!!);
+            Navigation.findNavController(it).popBackStack()
         }
-
-
 
     }
 }
