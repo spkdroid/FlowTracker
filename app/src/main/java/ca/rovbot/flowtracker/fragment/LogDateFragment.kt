1f80cd3e2c45c16f43b34aef8f7e7fab2a55a6d3
Ramkumar Velmurugan <vrk1990@outlook.com>
2018-12-26 11:44:08 -0500

    Log Period Implementation Completed

--- app/src/main/java/ca/rovbot/flowtracker/fragment/LogDateFragment.kt
+++ app/src/main/java/ca/rovbot/flowtracker/fragment/LogDateFragment.kt
@@ -1,5 +1,6 @@
 package ca.rovbot.flowtracker.fragment
 
+import android.app.DatePickerDialog
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import ca.rovbot.flowtracker.viewmodel.LogDateViewModel
 import ca.rovbot.flowtracker.R
+import kotlinx.android.synthetic.main.log_date_fragment.*
+import java.util.*
 
 
 class LogDateFragment : Fragment() {
@@ -29,6 +32,37 @@ class LogDateFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(LogDateViewModel::class.java)
         // TODO: Use the ViewModel
-    }
+        dateinputtext.setText(viewModel.getLastPeriodDate(context))
+        daysinputtext.setText(viewModel.getDays(context))
 
+        addImage.setOnClickListener{
+            val result = (Integer.parseInt(daysinputtext.text.toString())+1).toString();
+            daysinputtext.setText(result)
+        }
+
+        minusImage.setOnClickListener{
+            val result = (Integer.parseInt(daysinputtext.text.toString())-1).toString();
+            daysinputtext.setText(result)
+        }
+
+        dateperiodicon.setOnClickListener {
+            var c = Calendar.getInstance();
+            var mYear = c.get(Calendar.YEAR);
+            var mMonth = c.get(Calendar.MONTH);
+            var mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            val datePickerDialog = DatePickerDialog(
+                context,
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                    dateinputtext.setText(
+                        dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year
+                    )
+                },
+                mYear,
+                mMonth,
+                mDay
+            )
+            datePickerDialog.show()
+        }
+    }
 }
--- app/src/main/java/ca/rovbot/flowtracker/viewmodel/LogDateViewModel.kt
+++ app/src/main/java/ca/rovbot/flowtracker/viewmodel/LogDateViewModel.kt
@@ -1,7 +1,42 @@
 package ca.rovbot.flowtracker.viewmodel
 
 import android.arch.lifecycle.ViewModel;
+import android.content.Context
+import ca.rovbot.flowtracker.R
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LogDateViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun getLastPeriodDate(context: Context?): String {
+
+        var editor = context!!.getSharedPreferences(R.string.sharedpreferencename.toString(), Context.MODE_PRIVATE)
+         return editor.getString("freq","freq").toString()
+    }
+
+    fun getDays(context: Context?): String {
+        var editor = context!!.getSharedPreferences(R.string.sharedpreferencename.toString(), Context.MODE_PRIVATE)
+        return editor.getString("date","date").toString()
+    }
+
+
+    fun checkSelectedDateValid(context: Context?,selectedCalendarDate:String):String {
+        val editor = context!!.getSharedPreferences(R.string.sharedpreferencename.toString(), Context.MODE_PRIVATE)
+        val formatter: DateFormat
+        var date: Date
+        formatter = SimpleDateFormat("dd-MM-yyyy")
+        date = formatter.parse(selectedCalendarDate)
+        var currentDate = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date())
+        var currentDay: Date
+        currentDay = formatter.parse(currentDate)
+        val v = daysBetween(date,currentDay)
+        return v.toString();
+    }
+
+
+    fun daysBetween(d1: Date, d2: Date): Long {
+        return ((d2.time - d1.time) / (1000 * 60 * 60 * 24)) as Long
+    }
+
+
 }
